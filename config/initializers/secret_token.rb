541e94e74a36957408bbf094d1973de38bd910b1
Shaun Koo <shaunktw@gmail.com>
2013-12-26 21:19:21 -0500

    Added boostrap, and applied layout to the page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thefacebook::Application.config.secret_key_base = 'e4ff72c25277023ba8a0796204ff1cc1543cf2abdd81e0861cccfcdca927333d1233b7e539c25fc96d24db1e8ae44175eff41f1ddd4c3903ad9e76bcd80b727a'
